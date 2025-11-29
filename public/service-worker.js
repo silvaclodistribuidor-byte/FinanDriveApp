9f0b0f48eae1b75ac681c7df932f30fe57bb6abd
silvaclodistribuidor-byte <silvaclodistribuidor@gmail.com>
2025-11-29 20:29:35 -0300

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'finandrive-cache-v1';
+
+// Páginas/arquivos básicos para cache inicial
+const URLS_TO_CACHE = ['/', '/index.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Se tiver no cache, usa; se não, faz o fetch normal
+      return response || fetch(event.request);
+    })
+  );
+});
